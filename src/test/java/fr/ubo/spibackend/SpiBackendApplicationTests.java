7c638f4ff0d553365f5d0b7d2038e2c7d8da32c6
ayoubmazouni <ayoubmazouni@gmail.com>
2022-03-09 10:13:18 +0100

    init

---
+++ src/test/java/fr/ubo/spibackend/SpiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ubo.spibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
