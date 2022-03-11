7a113a95439be94a47ef557e064a6c9c1833779e
ayoubmazouni <ayoubmazouni@gmail.com>
2022-03-11 15:00:09 +0100

    ErrorHandling

--- src/main/java/fr/ubo/spibackend/controllers/PromotionController.java
+++ src/main/java/fr/ubo/spibackend/controllers/PromotionController.java
@@ -1,17 +1,21 @@
 package fr.ubo.spibackend.controllers;
 
 
-import fr.ubo.spibackend.entities.PromotionPK;
-import fr.ubo.spibackend.repositories.PromotionRepository;
 import fr.ubo.spibackend.entities.Promotion;
+import fr.ubo.spibackend.exception.RestErrorMessage;
+import fr.ubo.spibackend.exception.ServiceException;
 import fr.ubo.spibackend.services.PromotionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("promotion")
@@ -25,8 +29,18 @@ public class PromotionController {
         return promoServ.findAll();
     }
 
-    @GetMapping("/{annee}/{code}")
-    public Promotion findByAnneeUniversitaireAndCodeFormation(@PathVariable String annee, @PathVariable String code) {
-        return promoServ.findByAnneeUniversitaireAndCodeFormation(annee, code);
+    @GetMapping("/{code}/{annee}")
+    public ResponseEntity findById(@PathVariable String annee, @PathVariable String code) {
+        try {
+            return new ResponseEntity<Promotion>(promoServ.findById(annee, code), HttpStatus.OK);
+        }catch(ServiceException e){
+            return new ResponseEntity<RestErrorMessage>(new RestErrorMessage(e.getErrorMeassage()), e.getHttpStatus());
+        }catch (Exception e ){
+            return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/{code}")
+    public ArrayList<Promotion> findByCodeFormationOrderByAnneeUniversitaireDes(@PathVariable String code) {
+        return promoServ.findByCodeFormationOrderByAnneeUniversitaireDes(code);
     }
 }
